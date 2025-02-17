d3da0540efd8cd78f9b91b536ccef61b71df277a
YOGAYATA VERMA <63913693+yogayataverma@users.noreply.github.com>
2025-02-17 15:56:44 +0530

    Update DatastoreApplication.java

--- src/main/java/com/yogayataverma/datastore/DatastoreApplication.java
+++ src/main/java/com/yogayataverma/datastore/DatastoreApplication.java
@@ -2,12 +2,29 @@ package com.yogayataverma.datastore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DatastoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatastoreApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DatastoreApplication.class, args);
+  }
+  
+  @Configuration
+  public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+          registry.addMapping("/**")
+                  .allowedOrigins("*"); // You can restrict allowed origins if needed
+        }
+      };
+    }
+  }
 }
