f99abc377e8e6a8658e46c29eeee7092953622da
Yogayata Verma <yogayatajugnu@gmaiil.com>
2025-02-17 16:42:44 +0530

    changes

--- src/main/java/com/yogayataverma/datastore/DatastoreApplication.java
+++ src/main/java/com/yogayataverma/datastore/DatastoreApplication.java
@@ -7,32 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500")
 @SpringBootApplication
 public class DatastoreApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DatastoreApplication.class, args);
   }
-
-@Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*") // Allow all origins
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*") // Allow all headers
-                        .exposedHeaders("Access-Control-Allow-Origin") // Ensure this header is visible
-                        .allowCredentials(false);
-            }
-        };
-    }
-}
-
-
 }
