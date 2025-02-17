d2ad9abd37d7521dddeb95b476d3b02fa5b6e194
YOGAYATA VERMA <63913693+yogayataverma@users.noreply.github.com>
2025-02-17 16:05:31 +0530

    Update DatastoreApplication.java

--- src/main/java/com/yogayataverma/datastore/DatastoreApplication.java
+++ src/main/java/com/yogayataverma/datastore/DatastoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @SpringBootApplication
 public class DatastoreApplication {
 
