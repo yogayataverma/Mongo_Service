ee8b1d9d9075bda2911ddeb59f7b52254f7e8f09
YOGAYATA VERMA <63913693+yogayataverma@users.noreply.github.com>
2025-02-17 18:05:31 +0530

    Update Controller.java

--- src/main/java/com/yogayataverma/datastore/controller/Controller.java
+++ src/main/java/com/yogayataverma/datastore/controller/Controller.java
@@ -24,14 +24,7 @@ import com.yogayataverma.datastore.service.Service;
 // Controller.java
 @RestController
 @RequestMapping("/products")
-@CrossOrigin(
-    origins = {
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://your-frontend-domain.com"
-    },
-    allowCredentials = "true"
-)
+@CrossOrigin("*")
 public class Controller {
     @Autowired
     private Service service;
