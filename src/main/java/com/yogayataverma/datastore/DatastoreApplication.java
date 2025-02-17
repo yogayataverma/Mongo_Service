e10ee0802f27680b1f835a420fe197b85c1fb520
Yogayata Verma <yogayatajugn@gmail.com>
2025-02-17 16:49:36 +0530

    changes

--- src/main/java/com/yogayataverma/datastore/DatastoreApplication.java
+++ src/main/java/com/yogayataverma/datastore/DatastoreApplication.java
@@ -2,14 +2,10 @@ package com.yogayataverma.datastore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DatastoreApplication {
-
+ 
   public static void main(String[] args) {
     SpringApplication.run(DatastoreApplication.class, args);
   }
