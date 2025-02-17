12b724375688af43aa2bc000fdc747a9cac493b6
YOGAYATA VERMA <63913693+yogayataverma@users.noreply.github.com>
2025-02-17 16:28:35 +0530

    Update Controller.java

--- src/main/java/com/yogayataverma/datastore/controller/Controller.java
+++ src/main/java/com/yogayataverma/datastore/controller/Controller.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.Base64;
 import java.util.List;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500") // Allow frontend access
 @RestController
 @RequestMapping("/products")
 public class Controller {
